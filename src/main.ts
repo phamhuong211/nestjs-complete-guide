ca1dc792065e01d6890084dea2ed8f02137ed563
Pham Huong <phamhuong@MacBook-Air-cua-Pham.local>
2022-10-11 08:21:49 +0700

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe()
+  )
+  await app.listen(5000);
 }
 bootstrap();
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { createMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    @Get()
+    listMessages() {
+        
+    }
+
+    @Post()
+    createMessage(@Body() body: createMessageDto) {
+        console.log(body);
+    }
+
+    @Get(':id')
+    getMessage(@Param("id") id: string) {
+        console.log(id)
+    }
+}
